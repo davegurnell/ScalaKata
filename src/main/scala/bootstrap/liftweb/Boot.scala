6a089a9ee95f329fd4d97a20dccaff88f46e57d9
= <=>
2012-05-29 00:51:56 -0400

    now html5

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,12 +1,20 @@
 package bootstrap
 package liftweb
 
+import net.liftweb.common.Loggable
+
+import net.liftweb.util.Helpers._
+import net.liftweb.http.Html5Properties
 import net.liftweb.http.LiftRules
-import net.liftweb.sitemap.{SiteMap,Menu}
+import net.liftweb.http.Req
+
+import net.liftweb.sitemap.SiteMap
+import net.liftweb.sitemap.Menu
 
 class Boot {
   def boot {
     LiftRules.addToPackages("code")
+    LiftRules.htmlProperties.default.set((r: Req) => new Html5Properties(r.userAgent))
 		
     LiftRules.setSiteMap(SiteMap(Menu("Home") / "index"))
 
@@ -14,5 +22,9 @@ class Boot {
     LiftRules.statelessReqTest.append { case _ => true }
 
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
+    
+    // Use HTML5 for rendering
+	LiftRules.htmlProperties.default.set((r: Req) =>
+	  new Html5Properties(r.userAgent))  
   }
 }
