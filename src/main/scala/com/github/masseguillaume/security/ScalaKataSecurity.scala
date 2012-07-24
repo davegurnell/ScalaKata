20479ee0c2b33980258def8e1a17fc1e1cab6610
Guillaume Massé <masgui@gmail.com>
2012-07-24 16:18:33 -0400

    Security only in production

--- src/main/scala/com/github/masseguillaume/security/ScalaKataSecurity.scala
+++ src/main/scala/com/github/masseguillaume/security/ScalaKataSecurity.scala
@@ -1,11 +1,18 @@
 package com.github.masseguillaume.security
 
 import java.security._
+import net.liftweb.util.Props
 
 object ScalaKataSecurity {
 
 	def start = {
-		Policy.setPolicy( new ScalaKataSecurityPolicy )
-		System.setSecurityManager( new SecurityManager( ) )
+		
+		Props.mode match {
+			case Props.RunModes.Production => {
+				Policy.setPolicy( new ScalaKataSecurityPolicy )
+				System.setSecurityManager( new SecurityManager( ) )
+			} 
+			case _ => ()                                        
+		}
 	}
 }
