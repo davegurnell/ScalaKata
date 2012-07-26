d92e56f85e8ec96cffa28a737128c6ae318e37a7
Guillaume Massé <masgui@gmail.com>
2012-07-26 10:31:04 -0400

    FilePolicy read all ( XXX: Hack )
    On CloudBees I was not able to use external dependency, scalatest for
    example
    ( FilePermissionException read for WEB-INF/class/package )
    so this is a hack to allow read on all. I have to investigate to limit
    to this folder

--- src/main/scala/com/github/masseguillaume/security/ScalaKataSecurityPolicy.scala
+++ src/main/scala/com/github/masseguillaume/security/ScalaKataSecurityPolicy.scala
@@ -1,6 +1,7 @@
 package com.github.masseguillaume.security
 
 import java.security._
+import java.io.FilePermission
 
 import net.liftweb.common.LazyLoggable
 
@@ -10,18 +11,19 @@ import net.liftweb.common.LazyLoggable
  */
 class ScalaKataSecurityPolicy extends Policy
 {
-	private val allPermissions = new Permissions
-	allPermissions.add(new AllPermission)
+	private val websitePermissions = new Permissions
+	websitePermissions.add(new AllPermission)
 
-	private val noPermissions = new Permissions
-
+	private val scriptPermissions = new Permissions
+	scriptPermissions.add( new FilePermission("-","read") ) // all read
+	
 	override def getPermissions( codesource: CodeSource ) = {
-		if ( codesource.getLocation == null ) noPermissions
-		else allPermissions
+		if ( codesource.getLocation == null ) scriptPermissions
+		else websitePermissions
 	}
 
 	override def getPermissions( domain: ProtectionDomain ) = {
-		if ( domain.getCodeSource.getLocation == null ) noPermissions
-		else allPermissions
+		if ( domain.getCodeSource.getLocation == null ) scriptPermissions
+		else websitePermissions
 	}
 }
